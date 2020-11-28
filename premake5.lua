b7dc43390043abdf1704b04a06b83b3eab2356b4
Leo Naizin <leo.naizin@etudiant.univ-lr.fr>
2020-11-28 13:14:26 +0100

    Added context files to includes

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,7 @@ project "GLFW"
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
+        "src/context.c",
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
@@ -29,6 +30,9 @@ project "GLFW"
             "src/win32_time.c",
             "src/win32_thread.c",
             "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
         }
 
         defines
