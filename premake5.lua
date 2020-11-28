e5dfc620fd69188df3d489b9464fa424399e2224
Leo Naizin <leo.naizin@etudiant.univ-lr.fr>
2020-11-28 12:24:01 +0100

    Ajout d'un premake pour pouvoir compiler le code dans un autre projet

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+project "GLFW"
+    kind "StaticLib"
+    language "C"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/glfw_config.h",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/vulkan.c",
+        "src/window.c"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
+
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+        }
+
+        defines
+        {
+            "_GLFW_WIN32"
+        }
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
